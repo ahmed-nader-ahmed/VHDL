37634b6ab00cb1ad782dfc9e185268c0fa9d6e00
Ahmed Nader <111766208+ahmed-nader-ahmed@users.noreply.github.com>
2024-02-14 17:48:28 +0200

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -1,17 +1,16 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY ALU IS
-PORT( Op_code : IN STD_LOGIC_VECTOR( 2 DOWNTO 0 );
-A, B : IN STD_LOGIC_VECTOR( 31 DOWNTO 0 );
-Y : OUT STD_LOGIC_VECTOR( 31 DOWNTO 0 ) );
+   PORT( 
+      Op_code : IN STD_LOGIC_VECTOR( 2 DOWNTO 0 );
+      A, B : IN STD_LOGIC_VECTOR( 31 DOWNTO 0 );
+      Y : OUT STD_LOGIC_VECTOR( 31 DOWNTO 0 ) );
 END ALU;
 
 architecture ALU_arch of ALU is
-
-signal current_state, next_state : std_logic_vector(1 downto 0);
+   signal current_state, next_state : std_logic_vector(1 downto 0);
 begin
 
    comb_process : process (Op_code, A, B)
